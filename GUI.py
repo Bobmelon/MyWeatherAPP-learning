b07236b1de6beadb44b8b0b5433812ba5e17ef09
Bobmelon <tummadisetty@gmail.com>
2026-02-22 21:48:28 -0500

    .   changes to how GUI handles     window size and positon
    .   added to-do for labels

--- GUI.py
+++ GUI.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 
+windowWidth = 1100
+windowHeight = 600
+windowX = 400
+windowY = 200
+
+#window initialization
 root = tk.Tk()
 root.title("WeatherAPP")
-root.geometry("500x400")#   width x height
+root.geometry(f"{windowWidth}x{windowHeight}+{windowX}+{windowY}")#   width x height + X + Y
+
+#labels
+#. to do
 
 root.mainloop()
 
